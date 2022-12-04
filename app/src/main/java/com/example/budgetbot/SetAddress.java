d24efaa1dde97674701bfad2f1a2cf949646d606
Shaheer02 <shaheershoaib11@gmail.com>
2022-12-03 17:07:56 -0800

    Payment type

--- app/src/main/java/com/example/budgetbot/SetAddress.java
+++ app/src/main/java/com/example/budgetbot/SetAddress.java
@@ -11,7 +11,7 @@ public class SetAddress extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_address);
+        setContentView(R.layout.activity_address_set);
     }
 
     public void goBack(View view){
